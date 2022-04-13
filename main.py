9c9cbab68580bbf14ab641462591d57d6a81a223
andylushman <andylushman@gmail.com>
2022-04-13 15:52:15 +0200

    add post endpoint create_artist

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import csv
+from pydantic import BaseModel
 
 app = FastAPI()
+
+artists_file = 'artists.csv'
+
+
+class Artist(BaseModel):
+    name: str
+    location: Optional[str] = None
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/artists")
+def read_artists():
+    with open(artists_file, 'r') as csvfile:
+        data_reader = csv.reader(csvfile)
+        artists = [{"name": row[0], "location": row[1]} for row in data_reader]
+        return artists
+
+
+@app.post("/artists")
+def create_artist(artist: Artist):
+    with open(artists_file, 'a', newline="") as csvfile:
+        data_writer = csv.writer(csvfile)
+        data_writer.writerow([artist.name, artist.location])
 
 
 @app.get("/items/{item_id}")
